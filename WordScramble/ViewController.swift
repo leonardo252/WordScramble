36df6ae96ded465fb831e5e9c8dba3a2ed17649d
Leonardo Fernandes <leonardo.gomes@lit.ifce.edu.br>
2020-07-05 00:03:05 -0300

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Leonardo Gomes Fernandes on 05/07/20.
+//  Copyright © 2020 Leonardo Gomes Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
